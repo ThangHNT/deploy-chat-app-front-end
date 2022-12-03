6b2e3aa9d9449db61bbeef8749769b606d7d0321
ThangHNT <thanghn2503@gmail.com>
2022-12-03 10:18:25 +0700

    ud navigate to home and login page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,14 @@
-import { useContext, useEffect } from 'react';
+import {  useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '~/layouts/Header';
 import HomeContent from '~/layouts/HomeContent';
-import { UserContext } from '~/components/Context/UserContext';
 
 function Home() {
-    const { currentUser } = useContext(UserContext);
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (!currentUser) {
+        const user = JSON.parse(localStorage.getItem('chat-app-hnt'));
+        if (!user) {
             document.querySelector('html').classList.remove('darkmode');
             navigate('/login');
         }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,14 @@
-import { useEffect, useContext } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ContentLogin from '~/layouts/ContentLogin';
 import Header from '~/layouts/Header';
-import { UserContext } from '~/components/Context/UserContext';
 
 function Login() {
-    const { currentUser } = useContext(UserContext);
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (currentUser) {
+        const user = JSON.parse(localStorage.getItem('chat-app-hnt'));
+        if (user) {
             navigate('/');
         }
     });
